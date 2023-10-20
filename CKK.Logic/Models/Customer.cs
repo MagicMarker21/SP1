23ad86689a86c7926208bb1362886e5993cff8af
MagicMarker21 <144280628+MagicMarker21@users.noreply.github.com>
2023-10-20 11:47:47 -0600

    Add files via upload

---
+++ CKK.Logic/Models/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+   public class Customer
+    {
+        private int id;
+        private string name;
+        private string address; 
+
+        public int GetId()
+        {
+            return id;
+        }
+        public void SetId(int Id)
+        {
+            id = Id;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public void SetName(string Name)
+        {
+            name = Name;
+        }
+        public string GetAddress()
+        {
+            return address;
+        }
+        public void SetAddress(string Address)
+        {
+            address = Address;
+        }
+        
+
+    }
+}
---
+++ CKK.Logic/Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    internal class Product
+    {
+        private int id;
+        private string name;
+        private decimal price;
+
+        public int GetId()
+        {
+            return id;
+        }
+        public void SetId(int Id)
+        {
+            id = Id;
+        }
+        public string GetName()
+        {
+
+            return name;
+        }
+        public void SetName(string Name)
+        {
+            name = Name;
+        }
+        public decimal GetPrice()
+        {
+            return price;
+        }
+        public void SetPrice(decimal Price)
+        {
+            price = Price;
+        }
+    }
+}
